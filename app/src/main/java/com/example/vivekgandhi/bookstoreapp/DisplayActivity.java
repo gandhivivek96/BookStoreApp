f9e40b6f0da0c00881cbd607d92f6e8813f7c51d
Vivek Gandhi <vivek7772>
2017-03-17 18:40:51 +0530

    added displayactivity

--- app/src/main/java/com/example/vivekgandhi/bookstoreapp/DisplayActivity.java
+++ app/src/main/java/com/example/vivekgandhi/bookstoreapp/DisplayActivity.java
@@ -2,12 +2,138 @@ package com.example.vivekgandhi.bookstoreapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 
 public class DisplayActivity extends AppCompatActivity {
 
+     TextView n,a,p,s;
+    private TextView mResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
     }
+    public void displayData(View v)
+    {
+            n = (TextView) findViewById(R.id.bkname);
+            a = (TextView) findViewById(R.id.author);
+        p = (TextView) findViewById(R.id.price);
+        s = (TextView) findViewById(R.id.stock);
+        mResult = (TextView) findViewById(R.id.result);
+        //make GET request
+        new GetDataTask().execute("http://192.168.2.4:1001/api/status");
+    }
+
+    class GetDataTask extends AsyncTask<String, Void, String> {
+
+        ProgressDialog progressDialog;
+
+        @Override
+        protected void onPreExecute() {
+
+            super.onPreExecute();
+
+            progressDialog = new ProgressDialog(DisplayActivity.this);
+            progressDialog.setMessage("Loading data...");
+            progressDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            try {
+                return getData(params[0]);
+            } catch (IOException ex) {
+                return "Network error !";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            //set data response to textView
+            mResult.setText(result);
+
+            //cancel progress dialog
+            if (progressDialog != null) {
+                progressDialog.dismiss();
+            }
+        }
+
+        private String getData(String urlPath) throws IOException {
+            StringBuilder result = new StringBuilder();
+            BufferedReader bufferedReader =null;
+
+            try {
+                //Initialize and config request, then connect to server
+                URL url = new URL(urlPath);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setReadTimeout(10000 /* milliseconds */);
+                urlConnection.setConnectTimeout(10000 /* milliseconds */);
+                urlConnection.setRequestMethod("GET");
+                urlConnection.setRequestProperty("Content-Type", "application/json");// set header
+                urlConnection.connect();
+
+                //Read data response from server
+                InputStream inputStream = urlConnection.getInputStream();
+                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+
+                    result.append(line).append("\n");
+
+                }
+                //JSONParser parser = new JSONParser();
+
+                JSONObject reader = new JSONObject(result.toString());
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } finally {
+                if (bufferedReader != null) {
+                    bufferedReader.close();
+                }
+            }
+
+            return result.toString();
+        }
+    }
+
 }
